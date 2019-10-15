f06c3f83287259d83c9c3632745d6dfcce3980e9
Kylian <kylian.salomon@isen.yncrea.fr>
2019-10-15 11:28:09 +0200

    Ajout de Page menu affaire

--- geo_diagnostique_app/lib/MenuAffaire.dart
+++ geo_diagnostique_app/lib/MenuAffaire.dart
@@ -3,11 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MenuAffaire extends StatelessWidget{
-
-  @override
+  final List<ListTile> listNumeroAffaire = new List(5);
+  
   Widget build(BuildContext context){
+    listGenerator(context);
+
     return new Scaffold(
-      
-    )
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        title: Text("Menu d'affaire"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(5),
+        children: listNumeroAffaire,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.orangeAccent,
+        tooltip: 'Image',
+        child: Icon(Icons.add_box),
+      ),
+    );
+  }
+
+  void listGenerator( BuildContext context){
+    for(int i=0;i<5;i++){
+      listNumeroAffaire[i] = ListTile(
+        leading: Icon(Icons.filter_frames),
+        title: Text("Numero d'affaire $i"),
+        onTap: (){},
+
+      );
+    }
   }
 }
